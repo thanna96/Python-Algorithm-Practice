dad27aba9cbff85bacd5206e13b69b9d52a11ee4
thanna96 <thanna96@gmail.com>
2019-12-14 19:01:44 -0500

    queue done

--- Python_Practice/Queue.py
+++ Python_Practice/Queue.py
@@ -1,5 +1,28 @@
 '''
 Created on Nov 24, 2019
-
+=> Create a Queue with 2 Stacks 
 @author: thann
 '''
+
+class Queue:
+    
+        'Initialize List'
+        def __init__(self):
+            self.stack1 = []
+            self.stack2 = []
+            
+        'Insert Node in Back'
+        def enQueue(self, value):
+            while len(stack1 != 0):
+                self.stack2.append(self.stack1[-1])
+                self.stack1.pop()
+
+            self.stack1.append(value)
+
+            while len(self.stack2) != 0:
+                self.stack1.append(self.stack2[-1])
+                self.stack2.pop()
+        
+        'Remove Node in Front'
+        def deQueue(self):
+          
