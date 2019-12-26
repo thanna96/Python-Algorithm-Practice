e7b874d386884b778d94efb4881cc206e0ad807b
thanna96 <thanna96@gmail.com>
2019-12-26 15:59:54 -0500

    started tree

--- Python_Practice/Bin_Tree.py
+++ Python_Practice/Bin_Tree.py
@@ -1,5 +1,43 @@
 '''
 Created on Nov 24, 2019
-
+    => Creates Binary Search tree
 @author: thann
 '''
+
+class Node:
+
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Tree:
+
+    def __init__(self):
+            self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            if self.root.left is None and self.root.right is None:
+                self.root.left = Node(value)
+            elif self.root.left is not None and self.root.left.val <= value:
+                self.root.addRight(value)
+            else:
+                self.root.addLeft(value)
+
+    def addLeft():
+
+    def addRight():
+
+    def Post():
+
+    def Pre():
+
+    def In():
+
+    def remove():
+
+    def search();
+    
