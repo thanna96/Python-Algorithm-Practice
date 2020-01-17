6151a06f2d07e8855ac0a90c46a8b2384954ec5c
thanna96 <thanna96@gmail.com>
2020-01-16 21:00:44 -0500

    worked on tree

--- Python_Practice/Bin_Tree.py
+++ Python_Practice/Bin_Tree.py
@@ -11,33 +11,78 @@ class Node:
         self.left = None
         self.right = None
 
-class Tree:
-
-    def __init__(self):
-            self.root = None
-
-    def insert(self, value):
-        if self.root is None:
-            self.root = Node(value)
+def insert(root, node):
+        if root is None:
+            root = node
         else:
-            if self.root.left is None and self.root.right is None:
-                self.root.left = Node(value)
-            elif self.root.left is not None and self.root.left.val <= value:
-                self.root.addRight(value)
+            if root.value < node.value:
+                if root.right is None:
+                    root.right = node
+                else:
+                    insert(root.right, node)
             else:
-                self.root.addLeft(value)
+                if root.left is None:
+                    root.left = node
+                else:
+                    insert(root.left, node)
 
-    def addLeft():
+def In(root):
+    if root:
+        In(root.left)
+        print(root.val)
+        In(root.right)
 
-    def addRight():
+def Post(root):
+    if root:
+        Post(root.left)
+        Post(root.right)
+        print(root.value)
 
-    def Post():
+def Pre(root):
+   if root:
+        print(root.value)
+        Post(root.left)
+        Post(root.right)
 
-    def Pre():
+def Search(root, node):
+    if root:
+        if root is node:
+            return True
+        else:
+            remove(root.right, node)
+            remove(root.left, node)
 
-    def In():
+def minNode( node): 
+    cur = node 
+    while(cur.left is not None): 
+        cur = cur.left  
+    return cur 
 
-    def remove():
-
-    def search();
+def remove(root, key):
+    if root is None:
+        return root
     
+    if key < root.value:
+        root.left = remove(root.left, key)
+
+    elif: key > root.value:
+        root.right = remove(root.right, key)
+
+    else:
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+
+        temp = minNode(root.right)
+
+        root.value = temp.value 
+
+        root.right = remove(root.right, temp.value)
+
+    return root
+        
